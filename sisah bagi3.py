cb93ca3f3ffd8e3d6061ff0ecf7cdb155e7de40a
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2023-01-15 21:28:32 +0800

    Add files via upload

---
+++ sisah bagi3.py
@@ -0,0 +1,13 @@
+'''Buatlah program yang dapat menerima inputan berupa angka 
+dengan ketentuan sebagai berikut:
+1. Angka yang dimasukkan melalui input adalah batas atas 
+angka
+2. Program menampilkan jumlah angka yang habis dibagi 3
+Contoh angka yang diinput adalah 10, maka jawabannya 
+adalah 3, karena ada 3 angka yang habis dibagi 3 yaitu 3, 6 
+dan 9 '''
+
+angka = int(input("angka : " ))
+
+for i in range (3,angka+1,3):
+	print(i)
